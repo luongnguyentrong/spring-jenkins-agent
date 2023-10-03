ef3372d42a29afa71d85d17c5000d7570da38276
Luong in VDI <you@example.com>
2023-10-03 10:17:11 +0700

    test

--- env.groovy
+++ env.groovy
@@ -1,7 +1,7 @@
 env.NEXUS_HOST = "nexus.ducluong.co"
 
 env.NEXUS_DOCKER = "docker.luongntd1.lab.ocp.lan"
-env.NEXUS_DOCKER_UAT = "nexus.huynvb.lab.ocp.lan"
+env.NEXUS_DOCKER_UAT = "docker.luongntd1.lab.ocp.lan"
 env.NEXUS_DOCKER_MAIN = "${NEXUS_HOST}:9001"
 
 env.DEPLOY_HOST = "deploy.ducluong.co"
